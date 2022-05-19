b4ba0ae8c5ac459cf78afb71cdc2784879d0e527
shubham <shubh.shubham21@gmail.com>
2022-05-19 18:37:17 +0530

    intial commit

---
+++ src/pages/Contact.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PizzaLeft from '../assets/pizzaLeft.jpg';
+import '../styles/Contact.css';
+
+import { motion } from 'framer-motion/dist/framer-motion';
+
+function Contact() {
+	return (
+		<motion.div className='contact'>
+			<div
+				className='leftSide'
+				style={{ backgroundImage: `url(${PizzaLeft})` }}
+				initial={{ width: 0 }}
+				animate={{ width: '100%' }}
+				exit={{ x: window.innerWidth, transition: { duration: 0.1 } }}
+			></div>
+			<div className='rightSide'>
+				<h1> Contact Us</h1>
+
+				<form id='contact-form' method='POST'>
+					<label htmlFor='name'>Full Name</label>
+					<input name='name' placeholder='Enter full name...' type='text' />
+					<label htmlFor='email'>Email</label>
+					<input name='email' placeholder='Enter email...' type='email' />
+					<label htmlFor='message'>Message</label>
+					<textarea
+						rows='6'
+						placeholder='Enter message...'
+						name='message'
+						required
+					></textarea>
+					<button type='submit'> Send Message</button>
+				</form>
+			</div>
+		</motion.div>
+	);
+}
+
+export default Contact;
